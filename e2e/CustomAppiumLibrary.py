426e76f3cbb9b3ea05bd54bb473b600fe0cb50d8
justussoh <justus.soh@hotmail.com>
2020-08-04 22:40:48 +0800

    final

--- e2e/CustomAppiumLibrary.py
+++ e2e/CustomAppiumLibrary.py
@@ -11,9 +11,7 @@ class CustomAppiumLibrary(AppiumLibrary):
     # Open Deep Link
     def OpenDeepLink(self, url, package):
         self.TerminateApp(package)
-        # self._current_application().execute_script('mobile:deepLink',{'url': url, 'package': package})
         self._current_application().execute_script('mobile: shell',{'command': 'am start', "args": ["-W", "-a", "android.intent.action.VIEW", "-d", url.replace('&', '\&'), package]})
-        # self._current_application().get(url)
 
     # Reference Keywords
     def KeyboardType(self, textToType):
